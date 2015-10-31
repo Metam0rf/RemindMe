4ced5188272846f822e249bc7f683af6dc734e82
Eugene Zozulya <zozulyaeugene@gmail.com>
2015-10-31 20:11:14 +0200

    Created main activity and layout.

---
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.qoobico.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zozulia.ye on 31.10.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
